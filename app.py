18f8b2aafd677c58e4c7fcd3583ddb88ef882bca
Preetham Goud <preetham5a7@Preethams-MacBook-Air.local>
2025-04-29 19:11:41 -0400

    🔐 Fix password hashing method for registration

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def register():
         if User.query.filter_by(username=username).first():
             flash("Username already exists.")
             return redirect(url_for('register'))
-        hashed_password = generate_password_hash(password, method='sha256')
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
